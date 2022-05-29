3b94fcb242b25cebd2be247557ff9e38a078d6e2
Max Khotsin <slimgos22@gmail.com>
2022-05-29 18:01:13 +0300

    refactor and fix: rename classnames and ID's accoarding to BEM and rules 3.1-3.4 of html-and-css.md and fix ongoing issues

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incompleted-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -33,21 +33,23 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-list__item__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "task-list__item__checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-list__item__input input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="task-list__item__button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="task-list__item__button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = 'task-list__item__image';
     deleteButton.appendChild(deleteButtonImg);
 
-
+    listItem.className = 'task-list__item';
     //and appending.
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
@@ -85,7 +87,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +101,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
